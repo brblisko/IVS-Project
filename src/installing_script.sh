ab0a5199ee976b799104c9cfcb4576c62820abe8
brblisko <vesely.svk@gmail.com>
2021-04-30 02:23:16 +0200

    changed makefile and script to support new .desktop file and icon

--- src/installing_script.sh
+++ src/installing_script.sh
@@ -1,7 +1,9 @@
 mkdir -p ../install/usr/share/ttt-calc
 mkdir -p ../install/usr/local/bin
+mkdir -p ../install/usr/share/icons/ttt-calc
 cp main.py ../install/usr/share/ttt-calc
 cp mathlib_TTT.py ../install/usr/share/ttt-calc
 cp resources.py ../install/usr/share/ttt-calc
+cp icons8-ttt-calc.png ../install/usr/share/icons/ttt-calc
 ln -sf /usr/share/ttt-calc/main.py ../install/usr/local/bin/ttt-calc
 dpkg-deb --build ../install ../install/ttt-calc-installer.deb
